2816633d7bd19c55a830f4dd62026d71ed68a753
yman0794 <tykesam@yahoo.co.jp>
2023-01-21 05:22:20 +0900

    バリデーションを適用（エラーメッセージの修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, :family_name_kanji, :first_name_kanji, :family_name_kana,
+            :first_name_kana, :birthday, presence: true
+  validates :family_name_kanji, :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Input full-width characters' }
+  validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Input full-width katakana characters' }
+
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates_format_of :password, with: PASSWORD_REGEX, on: :create
 
