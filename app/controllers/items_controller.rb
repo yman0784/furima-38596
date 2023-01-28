4b144f890c3854b1e744c90552b638af4c3f51ad
yman0794 <tykesam@yahoo.co.jp>
2023-01-29 01:25:13 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
