58e9a2707467d76ba09fc76eba1497bdfa04b322
yman0794 <tykesam@yahoo.co.jp>
2023-01-30 16:06:31 +0900

    自身が出品していないとトップページへ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
       @items = Item.includes(:user).order("created_at DESC")
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to action: :index
+    end
   end
 
   def update
