f39e21c0fd1ea051e45f1b01c095b7f1af14b2fc
yman0794 <tykesam@yahoo.co.jp>
2023-02-07 02:43:06 +0900

    バリデーション修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -5,18 +5,17 @@ class PurchaseAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    # validates :price, numericality: {only_integer: true}
-    end
-
-    validates :postal_code, presence: { message: "can't be blank" }
-    validates :municipalities, presence: { message: "can't be blank" }
-    validates :address, presence: { message: "can't be blank" }
-    validates :building_name, presence: { message: "can't be blank" }
-    validates :telephone_number, presence: { message: "can't be blank" }
-    
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+  end
 
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :municipalities, presence: { message: "can't be blank" }
+  validates :address, presence: { message: "can't be blank" }
+  validates :telephone_number, presence: { message: "can't be blank" }
+  
+  validates :telephone_number, numericality: {only_integer: true, message: "is invalid. Input only number"}
+  validates :telephone_number, length: { minimum: 10, message: "is too short" }
 
   def save
     purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
